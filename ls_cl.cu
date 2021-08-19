9573e3e10aecd7b317828ce85130fad1075522a5
David Schramm <david.schramm@live.com>
2021-08-19 16:20:03 +0200

    change memory

--- ls_cl.cu
+++ ls_cl.cu
@@ -13,8 +13,8 @@ extern "C" {
 #include "parser.h"
 }
 
-#define RINGBUF_SIZE 16
-#define RINGBUF_MASK 15
+#define RINGBUF_SIZE 32
+#define RINGBUF_MASK 31
 
 static inline void check_error(cudaError_t e, const char *file, int line) {
     if(e != cudaSuccess) {
@@ -60,28 +60,30 @@ end:
     return NULL;
 }
 
-__global__ void ls(	const __restrict__ uint *lower, const __restrict__ uint *upper, const ulong num_rules, uint *header, uint *pos) {
+__global__ void ls(	const __restrict__ uint *lower, const __restrict__ uint *upper, const ulong num_rules, const __restrict__ uint *header, uint *pos) {
 
     uint start=(uint) blockDim.x*blockIdx.x+threadIdx.x, step=(uint) gridDim.x*blockDim.x;
     ulong bp;
     __shared__ uint8_t found;
-    if(!threadIdx.x)
-	    found=0;
+
+    if(!threadIdx.x){
+        found=0;
+	}
     __syncthreads();
 
     for(uint i=start; i<num_rules; i+=step) {
         bp=i<<3;
         if(lower[bp]<=header[0] & header[0]<=upper[bp]
-        	& lower[bp+1]<=header[1] & header[1]<=upper[bp+1]
-        	& lower[bp+2]<=header[2] & header[2]<=upper[bp+2]
-        	& lower[bp+3]<=header[3] & header[3]<=upper[bp+3]
-        	& lower[bp+4]<=header[4] & header[4]<=upper[bp+4]) {
+                & lower[bp+1]<=header[1] & header[1]<=upper[bp+1]
+                & lower[bp+2]<=header[2] & header[2]<=upper[bp+2]
+                & lower[bp+3]<=header[3] & header[3]<=upper[bp+3]
+                & lower[bp+4]<=header[4] & header[4]<=upper[bp+4]) {
             atomicMin(pos, i);
-	    found=1;
-	    __threadfence_system();
+            found=1;
+            __threadfence_system();
         }
-	if(found)
-		break;
+        if(found)
+            break;
     }
 }
 
@@ -112,13 +114,6 @@ bool ls_cl_new(ls_cl_t *lscl, const ruleset_t *rules, FILE *outfile) {
     CHECK(cudaMemcpy(lscl->upper, buffer, bufsize, cudaMemcpyHostToDevice));
 
     // head pos ring buffer
-
-    lscl->header_ring_copy_h=(uint32_t **) malloc(sizeof(uint32_t *)*RINGBUF_SIZE);
-    for(size_t i=0; i<RINGBUF_SIZE; ++i) {
-        lscl->header_ring_copy_h[i]=(uint32_t *) malloc(sizeof(uint32_t)<<3);
-        memset(lscl->header_ring_copy_h[i], 0, (sizeof(uint32_t)<<3));
-    }
-
     lscl->pos_ring_h=(uint32_t **) malloc(sizeof(uint32_t *)*RINGBUF_SIZE);
     lscl->pos_ring=(uint32_t **) malloc(sizeof(uint32_t *)*RINGBUF_SIZE);
     lscl->header_ring_h=(uint32_t **) malloc(sizeof(uint32_t *)*RINGBUF_SIZE);
@@ -147,7 +142,7 @@ bool ls_cl_new(ls_cl_t *lscl, const ruleset_t *rules, FILE *outfile) {
 
 void ls_cl_get(ls_cl_t *lscl, const header_t *header) {
     static uint32_t i=0;
-#define H(X) lscl->header_ring_copy_h[i][X-1]=header->h ## X
+#define H(X) lscl->header_ring_h[i][X-1]=header->h ## X
     H(1);
     H(2);
     H(3);
@@ -155,10 +150,7 @@ void ls_cl_get(ls_cl_t *lscl, const header_t *header) {
     H(5);
 #undef H
 
-    cudaMemcpyAsync(lscl->header_ring_h[i], lscl->header_ring_copy_h[i],
-                    sizeof(uint32_t)<<3, cudaMemcpyHostToDevice, lscl->streams[i]);
-
-    ls<<<lscl->mp_count,256,0,lscl->streams[i]>>>(lscl->lower, lscl->upper, (uint64_t) lscl->ruleset->num_rules,
+    ls<<<1,64,0,lscl->streams[i]>>>(lscl->lower, lscl->upper, (uint64_t) lscl->ruleset->num_rules,
             lscl->header_ring[i], lscl->pos_ring[i]);
 
     uint8_t stream_running;
@@ -184,11 +176,9 @@ void ls_cl_free(ls_cl_t *lscl) {
     for(size_t i=0; i<RINGBUF_SIZE; ++i) {
         cudaFreeHost(lscl->pos_ring_h[i]);
         cudaFreeHost(lscl->header_ring_h[i]);
-        free(lscl->header_ring_copy_h[i]);
     }
     free(lscl->pos_ring);
     free(lscl->header_ring);
-    free(lscl->header_ring_copy_h);
     free(lscl->pos_ring_h);
     free(lscl->header_ring_h);
     free(lscl->streams_running);
