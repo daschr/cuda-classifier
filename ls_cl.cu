e40bca409e92abf196dc4c0d8eba52e65b79110b
David Schramm <david.schramm@live.com>
2021-08-25 18:17:03 +0200

    return if no rules left

--- ls_cl.cu
+++ ls_cl.cu
@@ -73,7 +73,10 @@ __global__ void ls(	const __restrict__ uint *lower,  const __restrict__ uint *up
                 __threadfence_system();
             }
 
-            i+=step;
+            if((!start) & (i>rules_size))
+                found=1;
+            
+			i+=step;
             __syncthreads();
         }
 
